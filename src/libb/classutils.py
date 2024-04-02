5fe4cbf97bf51ed099a64807aaec96971ae0513e
bissli <bissli@xyz.com>
2024-04-02 15:33:25 +0000

    move composable, caller_entity functions to classutils

--- src/libb/classutils.py
+++ src/libb/classutils.py
@@ -2,6 +2,7 @@ import ast
 import inspect
 import logging
 import sys
+from collections.abc import Iterable
 from functools import wraps
 
 logger = logging.getLogger(__name__)
@@ -362,6 +363,107 @@ def find_decorators(target):
     V.visit_FunctionDef = visit_function_def
     V.visit(compile(inspect.getsource(target), '?', 'exec', ast.PyCF_ONLY_AST))
     return res
+
+
+def composable(decorators):
+    """Decorator that takes a list of decorators to be composed
+
+    useful when list of decorators starts getting large and unruly
+
+    >>> def m3(func):
+    ...     def wrapped(n):
+    ...         return func(n)*3.
+    ...     return wrapped
+
+    >>> def d2(func):
+    ...     def wrapped(n):
+    ...         return func(n)/2.
+    ...     return wrapped
+
+    >>> def p3(n):
+    ...     return n+3.
+
+    >>> @m3
+    ... @d2
+    ... def plusthree(x):
+    ...     return p3(x)
+
+    >>> @composable([d2, m3])
+    ... def cplusthree(x):
+    ...     return p3(x)
+
+    Despite the similar name, composed decorators are not
+    interchangeable with `compose` for standard functions,
+    since decorators return functions, not the func output
+    >>> func = compose(m3, d2, p3)(4)
+    >>> hasattr(func, '__call__')
+    True
+    >>> compose(lambda n: n*3., lambda n: n/2., p3)(4)
+    10.5
+
+    what they do allow is consolidating longer decorator chains
+    >>> plusthree(4)
+    10.5
+    >>> cplusthree(4)
+    10.5
+    """
+
+    def composed(func):
+        if isinstance(decorators, Iterable) and not isinstance(decorators, str):
+            for dec in decorators[::-1]:
+                func = dec(func)
+            return func
+        return decorators(func)
+
+    def wrapped(func):
+        @wraps(func)
+        def f(*a, **kw):
+            return composed(func)(*a, **kw)
+        return f
+
+    return wrapped
+
+
+def caller_entity(func):
+    """Helper to get current entity from function"""
+    # general frame args inspect
+    frame = inspect.currentframe()
+    outer_frames = inspect.getouterframes(frame)
+    caller_frame = outer_frames[1][0]
+    args = inspect.getargvalues(caller_frame)
+    # find our entity
+    param = inspect.signature(func).parameters.get('entity')
+    default = param.default if param else NYSE
+    entity = args.locals['kwargs'].get('entity', default)
+    return entity
+
+
+def catch_exception(f):
+    @wraps(f)
+    def func(*args, **kwargs):
+        try:
+            return f(*args, **kwargs)
+        except Exception as e:
+            print('Caught an exception in', f.__name__)
+    return func
+
+
+class ErrorCatcher(type):
+    """
+    >>> class Test(metaclass=ErrorCatcher):
+    ...     def __init__(self, val):
+    ...         self.val = val
+    ...     def calc(self):
+    ...         return self.val / 0
+    >>> t = Test(5)
+    >>> t.calc()
+    Caught an exception in calc
+    """
+    def __new__(cls, name, bases, dct):
+        for m in dct:
+            if callable(dct[m]):
+                dct[m] = catch_exception(dct[m])
+        return type.__new__(cls, name, bases, dct)
 
 
 if __name__ == '__main__':
