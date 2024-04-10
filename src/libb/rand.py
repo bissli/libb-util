3b2c1b114ddf002a968954e335ff41b737a73b92
bissli <bissli@xyz.com>
2024-04-10 13:57:30 +0000

    add random_int to rand

--- src/libb/rand.py
+++ src/libb/rand.py
@@ -1,29 +1,41 @@
 import logging
 import os
 import random
+from functools import wraps
 
 import numpy as np
 from typing_extensions import List
 
 logger = logging.getLogger(__name__)
 
-
-def randomseed() -> int:
-    """Random seed through the OS
-    """
-    RAND_SIZE = 4
-    random_data = os.urandom(RAND_SIZE)
-    random_seed: int = int.from_bytes(random_data, byteorder='big')
-    return int(random_seed)
+__all__ = ['random_choice', 'random_int', 'random_sample']
 
 
+def rseed(func):
+    """Random seed through the OS"""
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        RAND_SIZE = 4
+        random_data = os.urandom(RAND_SIZE)
+        random_seed = int.from_bytes(random_data, byteorder='big')
+        random.seed(random_seed)
+        return func(*args, **kwargs)
+    return wrapper
+
+
+@rseed
 def random_choice(choices: List):
     """Random choice amont list of choices"""
-    random.seed(randomseed())
     random.shuffle(choices)
     return choices[0]
 
 
+@rseed
+def random_int(a: int, b: int):
+    return random.randint(a, b)
+
+
+@rseed
 def random_sample(arr: np.array, size: int = 1) -> np.array:
     """Random sample size N element from numpy array"""
     return arr[np.random.choice(len(arr), size=size, replace=False)]
