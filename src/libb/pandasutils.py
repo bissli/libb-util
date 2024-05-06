61913c38bd62296b82d93bd62b6d7577ef8fb698
bissli <bissli@xyz.com>
2024-05-06 14:51:24 +0000

    update is_null to include iterables

--- src/libb/pandasutils.py
+++ src/libb/pandasutils.py
@@ -1,4 +1,5 @@
 import contextlib
+from collections.abc import Iterable
 
 import numpy as np
 
@@ -12,6 +13,10 @@ __all__ = ['is_null']
 def is_null(x):
     """Simple null/none checker
     """
+    if isinstance(x, Iterable):
+        with contextlib.suppress(Exception):
+            return np.all(x.isna())
+        return bool(x)
     with contextlib.suppress(Exception):
         if isinstance(x, pd._libs.missing.NAType):
             return True
