57b7eb0123921f15c10f6e0731eb2e3b09a83177
bissli <bissli@xyz.org>
2026-08-05 12:21:17 -0400

    Track Cargo.lock and switch Sphinx napoleon to NumPy docstrings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,8 +23,8 @@ extensions = [
     'myst_parser',
 ]
 
-napoleon_google_docstring = True
-napoleon_numpy_docstring = False
+napoleon_google_docstring = False
+napoleon_numpy_docstring = True
 napoleon_include_init_with_doc = True
 napoleon_include_private_with_doc = False
 napoleon_include_special_with_doc = True
